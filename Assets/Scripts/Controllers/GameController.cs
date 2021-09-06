705beede9e7f4392a32fdcfd61070ccb51e81755
Chillu1 <lukasz20002604@gmail.com>
2021-09-06 23:33:48 +0200

    fix: base project update, small new test changes

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -29,23 +29,23 @@ namespace ComboSystem
             ModifierPrototypes = new ModifierPrototypes();
             ComboModifierPrototypes = new ComboModifierPrototypes();
 
-            player = new Character("Player", 5, 50);
+            //player = new Character("Player", 5, 50);
             //Modifier mod1 = ModifierPrototypes.GetModifier("MovementSpeedOfCat");
             //player.AddModifier(mod1);
             //Modifier mod2 = ModifierPrototypes.GetModifier("AttackSpeedOfCat");
             //player.AddModifier(mod2);
             //player.ModifierController.ListModifiers();
 
-            Modifier fireDamage = ModifierPrototypes.GetModifier("FireDamage");
-            fireDamage.AddCondition(delegate(Modifier modifier)
-            {
-                return modifier.Target?.StatusEffects.HasFlag(StatusEffect.Stunned) == true;//TODO Test
-            });
-            Modifier coldDamage = ModifierPrototypes.GetModifier("ColdDamage");
-            player.AddModifier(fireDamage);
-            player.AddModifier(coldDamage);
+            //Modifier fireDamage = ModifierPrototypes.GetModifier("FireDamage");
+            //fireDamage.AddCondition(delegate(Modifier modifier)
+            //{
+            //    return modifier.Target?.StatusEffects.HasFlag(StatusEffect.Stunned) == true;//TODO Test
+            //});
+            //Modifier coldDamage = ModifierPrototypes.GetModifier("ColdDamage");
+            //player.AddModifier(fireDamage);
+            //player.AddModifier(coldDamage);
 
-            Character slime = new Character("Slime", 3, 20);
+            //Character slime = new Character("Slime", 3, 20);
             //var slimePoisonBuff = (ModifierApplier<ModifierApplierData>)ModifierPrototypes.GetModifier<ModifierApplierData>("SlimePoisonBuff");
             //slime.AddModifier(slimePoisonBuff, AddModifierParameters.NullStartTarget);
             //slimePoisonBuff.ApplyModifierToTarget(player);
@@ -65,7 +65,7 @@ namespace ComboSystem
                 TimeController.Update();
             }
             CommandsController.Update(Time.deltaTime);
-            player.ModifierController.Update(Time.deltaTime);
+            //player.ModifierController.Update(Time.deltaTime);
         }
     }
 
